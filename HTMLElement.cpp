e8c68f43e52775857c0bb1b20b042abc71e4e45c
dgski <gorski.dave@gmail.com>
2019-01-23 21:15:41 -0500

    Initial commit

---
+++ HTMLElement.cpp
@@ -0,0 +1,83 @@
+#include "HTMLElement.h"
+
+using namespace std;
+
+HTMLElement::HTMLElement(string _tag, string _textContent)
+{
+    tag = _tag;
+    textContent = _textContent;
+}
+
+HTMLElement& HTMLElement::setAttribute(const char*  attr, const char* val)
+{
+    attributes[attr] = val;
+    return *this;
+}
+
+void HTMLElement::setSingle(bool _single)
+{
+    single = _single;
+}
+
+HTMLElement& HTMLElement::appendChild(const HTMLElement& child)
+{
+    contents.push_back(child);
+
+    HTMLElement& justAdded = contents.back();
+    justAdded.setParent(this);
+
+    return justAdded;
+}
+
+void HTMLElement::generateContents(ostream& stream) const
+{
+    if(textContent.length())
+        stream << textContent;
+
+    for(const HTMLElement& e : contents)
+        e.generate(stream);
+}
+
+void HTMLElement::generateAttributes(ostream& stream) const
+{
+    if(!attributes.empty())
+        stream << " ";
+
+    for(const auto& [key,value] : attributes)
+        stream << key  << "='" << value << "' ";
+}
+
+void HTMLElement::generate(ostream& stream) const
+{   
+    if(tag == "text" || tag == "blank")
+    {
+        generateContents(stream);
+        return;
+    }
+
+    stream << "<" << tag;
+    generateAttributes(stream);
+    stream << ">";
+
+    if(!single)
+    {
+        generateContents(stream);
+        stream << "</" << tag << ">";
+    }
+}
+
+HTMLElement* HTMLElement::getParent() const
+{
+    return parent;
+}
+
+void HTMLElement::setParent(HTMLElement* _parent)
+{
+    parent = _parent;
+}
+
+ostream& operator<<(ostream& stream, const HTMLElement& elem)
+{
+    elem.generate(stream);
+    return stream;
+}
---
+++ HTMLElement.h
@@ -0,0 +1,31 @@
+#include <string>
+#include <list>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+class HTMLElement
+{
+    string tag;
+    string textContent;
+    HTMLElement* parent =  nullptr;
+    list<HTMLElement> contents;
+    map<string,string> attributes;
+    bool single = false; // single tag ex: <br>
+
+    void generateContents(ostream& stream) const;
+    void generateAttributes(ostream& stream) const;
+    void setParent(HTMLElement* _parent);
+public:
+    HTMLElement(string _tag, string _textContent = "");
+    HTMLElement& setAttribute(const char* attr, const char* val);
+    void setSingle(bool _single);
+    HTMLElement& appendChild(const HTMLElement& child);
+    HTMLElement* getParent() const;
+    void generate(ostream& stream) const;
+};
+
+ostream& operator<<(ostream& stream, const HTMLElement& elem);
+
+#pragma once
